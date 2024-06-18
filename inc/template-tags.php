9e008422a322b6859f3ad28036e3877eca854eae
Manuel Ziel <ManuelZiel@gmail.com>
2024-06-18 21:22:42 +0200

    2.3.0

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -412,3 +412,13 @@ if ( ! function_exists( 'lilo_pagination' ) ) :
 
 	}
 endif;
+
+if ( ! function_exists( 'lilo_related_posts' ) ) :
+	/**
+	 * Displays related posts on single post view
+	 */
+	function lilo_related_posts() {
+
+		
+	}
+endif;
